0601ef1999bdea73d6ec5843d85484d7ec9e2c9b
Erik Peterson <epeterson@concentrichx.com>
2022-02-22 11:22:58 -0500

    Ramadan, Experiences, City Break EN

--- src/global/js/main.js
+++ src/global/js/main.js
@@ -4,7 +4,7 @@ function enableRollOvers() {
   rollover_hit.addEventListener('mouseover', function () {
     //cta.style.willChange = 'transform';
     gsap.to(cta, 0.33, {
-      scale: 0.54,
+      scale: 0.55,
       force3D: false,
       rotation: 0.1,
       ease: Power1.easeIn,
